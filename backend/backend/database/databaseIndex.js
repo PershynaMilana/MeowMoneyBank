dd9632aa3e4c8c01a42ff7fc6ab8d34e1b47c7c9
Milana <mimilik07@gmail.com>
2023-12-18 22:20:32 +0200

    commit

--- backend/backend/database/databaseIndex.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = async() => {
-    mongoose.connect(`mongodb+srv://DanilArtemNazar:8hYR81TKAfMggDFj@atlascluster.opbt4mf.mongodb.net/`)
-    .then(() => {console.log("Connect to MongoDB")})
-    .catch(err => console.log(err))
-}
-
-
--- backend/backend/routes/routerSI.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const SourceIncome = require('../controllers/SourcesIncome')
-
-router.post('/addSourceIncome', SourceIncome.addSourceIncome)
-
-module.exports = router
-
