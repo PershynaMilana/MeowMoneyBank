ae3855a650136ca4901e24651e14e36d4e34d963
silvertxd <silvertxd@gmail.com>
2023-12-19 21:18:30 +0200

    Test

--- frontend/meowbank/meowbankfront/src/App.jsx
+++ frontend/meowbank/meowbankfront/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <div>
-        <h1 className='text-red-500'>ASD</h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1 className='text-red-500'>Test</h1>
+		</div>
+	)
 }
 
 export default App
